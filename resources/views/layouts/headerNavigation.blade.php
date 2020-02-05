06b86af881c62ede426490fc21d63321eb8f70ab
Aleksandr Gridasov <gridass2054@gmail.com>
2020-02-05 15:46:29 +0200

    fix adminbar

--- resources/views/layouts/headerNavigation.blade.php
+++ resources/views/layouts/headerNavigation.blade.php
@@ -1,8 +1,11 @@
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
 
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -35,7 +38,6 @@
                                                      document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
-
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                 @csrf
                             </form>
